306fb6085109f89fc35773fa6405c986f83da9c0
SJarygin <SJarygin@gmail.com>
2019-07-13 11:02:54 +0300

    = fix debug output

--- static/javascripts/main.js
+++ static/javascripts/main.js
@@ -45,36 +45,36 @@ function LogJson(AJsonData) {
   console.log(AJsonData);
   //log.value = JSON.stringify(AJsonData, null, '\t');
 
-  AJsonData =
-      {
-        "date":
-            "2019-07-07T11:49:39.145Z",
-        "message":
-            "ready",
-        "conf":
-            "3500",
-        "conf_full":
-            "3500-165.22.143.0",
-        "list":
-            [{
-              "number": "1000",
-              "title": "sipml.client.1000",
-            },],
-        "history":
-            [
-              {
-                "number": "1000",
-                "title": "sipml.client.1000",
-                "status": "connect",
-                "date": "2019-07-07T11:25:28.281Z"
-              },
-              {
-                "number": "1008",
-                "title": "SipMedia.1008",
-                "status": "connect",
-                "date": "2019-07-07T11:25:42.187Z"
-              }]
-      }
+  // AJsonData =
+  //     {
+  //       "date":
+  //           "2019-07-07T11:49:39.145Z",
+  //       "message":
+  //           "ready",
+  //       "conf":
+  //           "3500",
+  //       "conf_full":
+  //           "3500-165.22.143.0",
+  //       "list":
+  //           [{
+  //             "number": "1000",
+  //             "title": "sipml.client.1000",
+  //           },],
+  //       "history":
+  //           [
+  //             {
+  //               "number": "1000",
+  //               "title": "sipml.client.1000",
+  //               "status": "connect",
+  //               "date": "2019-07-07T11:25:28.281Z"
+  //             },
+  //             {
+  //               "number": "1008",
+  //               "title": "SipMedia.1008",
+  //               "status": "connect",
+  //               "date": "2019-07-07T11:25:42.187Z"
+  //             }]
+  //     }
 
   let cnt = 1;
   const connectedList = AJsonData.list.map(AItem => `${cnt++}) ${AItem.number} (${AItem.title})`).join('\n');
@@ -96,8 +96,8 @@ Connected:
 ${connectedList}
 
 History(order by date desc):
-${historyList}
-${JSON.stringify(AJsonData, null, '\t')}`;
+${historyList}`;
+//${JSON.stringify(AJsonData, null, '\t')}`;
 
   // const xx =
   //     {
